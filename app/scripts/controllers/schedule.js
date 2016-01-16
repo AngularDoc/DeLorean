3949607dbb9f51e7292e7fee37e419584e2d1f5b
GDG Kansas City <gdgkansascity@gmail.com>
2016-01-16 05:39:15 -0600

    Revamp session schedule look

--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -8,24 +8,26 @@
  * Controller of the devfestApp
  */
 angular.module('devfestApp')
-  .controller('ScheduleCtrl', function($scope, Ref, $firebaseArray, $timeout, $uibModal, $window, $location, $confirm) {
+  .controller('ScheduleCtrl', function($scope, Ref, $firebaseArray, $timeout, $uibModal, $window, $location, Config) {
     $scope.sessions = $firebaseArray(Ref.child('sessions'));
-    $scope.tab = 1;
-
-    $scope.setTab = function(newTab) {
-      $scope.tab = newTab;
-    };
-
-    $scope.isTab = function(tabNum) {
-      return $scope.tab === tabNum;
-    };
-
+    
+    $scope.sessionTimes = [];
+    Ref.child('sessions').orderByChild('time').on('value', function(snap) {
+      var sessions = snap.val(), x;
+      for (x in sessions) {
+        var time = sessions[x].time;
+        if ($scope.sessionTimes.indexOf(time) == -1) {
+          $scope.sessionTimes.push(time);
+        }
+      }
+    });
+    
     $scope.openFormModal = function(session) {
       $scope.session = session;
       var modalInstance = $uibModal.open({
         animation: true,
         templateUrl: 'modalSessionForm.html',
-        controller: 'SessionModalCtrl',
+        controller: 'SessionFormModalCtrl',
         resolve: {
           session: function() {
             return $scope.session;
@@ -41,23 +43,56 @@ angular.module('devfestApp')
       });
     };
 
+    $scope.openInfoModal = function(session) {
+      $scope.session = session;
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'modalSessionContent.html',
+        controller: 'SessionModalCtrl',
+        resolve: {
+          session: function() {
+            return $scope.session;
+          }
+        }
+      });
+      modalInstance.result.then(function(results) {
+        if (results.action === 'delete') {
+          $scope.delete(results.session);
+        } else if (results.action === 'edit') {
+          $scope.openFormModal(results.session);
+        }
+      });
+    };
+    
+    $scope.getTime = function(time) {
+      var sHour = time.substring(0, time.indexOf(':'));
+      var sMinutes = time.substring(time.indexOf(':')+1, time.indexOf(':')+3);
+      var event = parseDate(Config.eventDate);
+      return new Date(event.getFullYear(), event.getMonth(), event.getDate(), sHour, sMinutes, 0);
+    };
+    
+    $scope.getEndTime = function(time) {
+      var sessionTime = $scope.getTime(time);
+      var newTime = sessionTime.getTime();
+      newTime += parseInt(Config.sessionLength);
+      return new Date(newTime);
+    };
+    
+    function parseDate(str) {
+      var d = str.match(/^(\d{4})-(\d{1,2})-(\d{1,2})$/);
+      return (d) ? new Date(d[1], d[2]-1, d[3]) : new Date();
+    }
+
     $scope.add = function(session) {
       $scope.sessions.$add(session);
-    };
-
-    $scope.editSession = function(session) {
-      $scope.openFormModal(session);
     };
 
     $scope.edit = function(session) {
       $scope.sessions.$save(session);
     };
   
-    $scope.deleteSession = function(session) {
-      $confirm({text: 'Are you sure you want to delete ' + session.title + '? (this cannot be undone)'})
-        .then(function() {
-          $scope.sessions.$remove(session);
-        });
+    $scope.delete = function(session) {
+      $scope.sessions.$remove(session);
     };
     
     $scope.$on('$viewContentLoaded', function() {
@@ -67,13 +102,13 @@ angular.module('devfestApp')
 
 /**
  * @ngdoc function
- * @name devfestApp.controller:SessionModalCtrl
+ * @name devfestApp.controller:SessionFormModalCtrl
  * @description
- * # SessionModalCtrl
+ * # SessionFormModalCtrl
  * Controller of the devfestApp
  */
 angular.module('devfestApp')
-  .controller('SessionModalCtrl', function($scope, Ref, $firebaseArray, $uibModalInstance, session) {
+  .controller('SessionFormModalCtrl', function($scope, Ref, $firebaseArray, $uibModalInstance, session) {
     $scope.speakers = $firebaseArray(Ref.child('speakers'));
     $scope.session = session;
     $scope.err = null;
@@ -92,6 +127,40 @@ angular.module('devfestApp')
       } else {
         $scope.err = 'Please fill out the form or click Cancel to close.';
       }
+    };
+    
+    $scope.cancel = function() {
+      $uibModalInstance.dismiss('cancel');
+    };
+  });
+
+/**
+ * @ngdoc function
+ * @name devfestApp.controller:SessionModalCtrl
+ * @description
+ * # SessionModalCtrl
+ * Controller of the devfestApp
+ */
+angular.module('devfestApp')
+  .controller('SessionModalCtrl', function($scope, Ref, $firebaseArray, $uibModalInstance, $confirm, session) {
+    $scope.speakers = $firebaseArray(Ref.child('speakers'));
+    $scope.session = session;
+
+    $scope.editSession = function(session) {
+      $uibModalInstance.close({
+        'action': 'edit',
+        'session': session
+      });
+    };
+
+    $scope.deleteSession = function(session) {
+      $confirm({text: 'Are you sure you want to delete "' + session.title + '"? (this cannot be undone)'})
+        .then(function() {
+          $uibModalInstance.close({
+            'action': 'delete',
+            'session': session
+          });
+        });
     };
     
     $scope.cancel = function() {
