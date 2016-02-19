6528b7c3a192c42f964459d954e12892829627ac
GDG Kansas City <gdgkansascity@gmail.com>
2016-02-19 12:34:37 -0600

    Add survey and video link fields & buttons

--- app/scripts/controllers/schedule.js
+++ app/scripts/controllers/schedule.js
@@ -146,9 +146,31 @@ angular.module('devfestApp')
     $scope.speakers = $firebaseArray(Ref.child('speakers'));
     $scope.session = session;
     var postText = 'Check out this talk "' + session.title + '" only at #' + Config.eventName.replace(/ /g, '');
+    var today = new Date();
+    
+    function parseDate(str) {
+      var d = str.match(/^(\d{4})-(\d{1,2})-(\d{1,2})$/);
+      return (d) ? new Date(d[1], d[2]-1, d[3]) : new Date();
+    }
+
+    var event = parseDate(Config.eventDate);
+    var eventDate = new Date(event.getFullYear(), event.getMonth(), event.getDate(), 0, 0, 0);
+    var todayDate = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0);
+    $scope.event = eventDate.getTime();
+    $scope.today = todayDate.getTime();
+  
+    $scope.showSurvey = $scope.today == $scope.event;
     
     $scope.shareTwitter = function(session) {
       $window.open('//twitter.com/share?text=' + encodeURIComponent(postText) + '&url=' + Config.eventURL, '_blank');
+    };
+    
+    $scope.openSurvey = function(session) {
+      $window.open(session.surveyLink, '_blank');
+    };
+    
+    $scope.openVideo = function(session) {
+      $window.open(session.videoLink, '_blank');
     };
 
     $scope.editSession = function(session) {
